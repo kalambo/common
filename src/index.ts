230ca4e32d117f2e0a1b3c8007cb2b9bf7942e01
Jon Whitehead <jsiwhitehead@gmail.com>
2017-11-22 00:38:12 +0000

    Removed server code

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
-import * as enhance from './enhance';
-export { enhance };
 export { default as isValid } from './isValid';
 export { decodeId, encodeId } from './shortIds';
-export { default as sqlResolver } from './sqlResolver';
-export { default as storage } from './storage';
 export { getValueString, noUndef, transformValue } from './utils';
 
 declare global {
