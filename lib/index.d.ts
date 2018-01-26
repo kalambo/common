e703b52a676893c2d161eb72ecaddbb3a138f9de
Jon Whitehead <jsiwhitehead@gmail.com>
2018-01-26 12:46:16 +0000

    Added generic global root export

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,21 @@
+import { Rgo } from 'rgo';
 export { default as isValid } from './isValid';
 export { decodeId, encodeId } from './shortIds';
 export { Obj } from './typings';
 export { getValueString, noUndef, transformValue } from './utils';
+export declare const root: Window | Global;
+declare global  {
+    interface Window {
+        rgo: Rgo;
+    }
+    interface Global {
+        rgo: Rgo;
+    }
+}
+declare global  {
+    namespace NodeJS {
+        interface Global {
+            rgo: Rgo;
+        }
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,29 @@
+import { Rgo } from 'rgo';
+
 export { default as isValid } from './isValid';
 export { decodeId, encodeId } from './shortIds';
 export { Obj } from './typings';
 export { getValueString, noUndef, transformValue } from './utils';
+
+export const root =
+  typeof self !== 'undefined'
+    ? self
+    : typeof window !== 'undefined'
+      ? window
+      : typeof global !== 'undefined' ? global : ({} as Window | Global);
+
+declare global {
+  interface Window {
+    rgo: Rgo;
+  }
+  interface Global {
+    rgo: Rgo;
+  }
+}
+declare global {
+  namespace NodeJS {
+    interface Global {
+      rgo: Rgo;
+    }
+  }
+}
